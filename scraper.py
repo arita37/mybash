20f8775c6e72fbf90f77ac96e1df5f3f76fce278
PN <arita37@users.noreply.github.com>
2023-05-03 20:15:37 +0900

    ok

--- scraper.py
+++ scraper.py
@@ -6,11 +6,10 @@ python scraper.py  fetch  --query "simple logo bicycle in black and white" --lim
 
 
 '''
-import fire, os, sys,re, urllib
+import fire, os, sys,re, urllib,time, requests, io
 from pathlib import Path
 import urllib.request
-import imghdr
-import posixpath
+import imghdr, posixpath
 
 from rembg import remove
 from utilmy import log, os_makedirs, glob_glob
@@ -21,15 +20,180 @@ except: pass
 
 
 #import helper libraries
-import time
-import urllib.request
 from urllib.parse import urlparse
-import os
-import requests
-import io
 from PIL import Image
-import re
 
+
+
+################################################################################################
+def fetch_google(query, image_path="downloads", limit=10, headless=True, min_resolution=(0, 0), 
+                 max_resolution=(512, 512), max_missed=10):
+    """
+        pip install selenium
+        https://chromedriver.chromium.org/downloads
+
+    """             
+    from selenium import webdriver
+    from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.support import expected_conditions as EC
+    from selenium.common.exceptions import NoSuchElementException 
+
+    googleI = GoogleImageScraper(query, image_path, limit, headless, min_resolution, max_resolution, max_missed)
+    googleI.download()
+    del googleI
+
+
+
+def fetch(query, limit=1, dirout='dl', adult_filter='on', 
+          force_replace=False, timeout=60, filter="", verbose=0, png_conv=1):
+
+    # engine = 'bing'
+    
+    image_dir = Path(dirout).joinpath(query).absolute()
+    os_makedirs(str(image_dir))
+
+    png_conv = True if png_conv == 1 else False 
+    verbose  = True if verbose  == 1 else False 
+
+        
+    print("[%] Downloading Images to {}".format(str(image_dir.absolute())))
+    bing = Bing(query, limit, image_dir, adult_filter, timeout, filter, verbose, png_conv)
+    bing.run()
+
+
+
+
+
+
+################################################################################################
+class Bing:
+    def __init__(self, query, limit, dirout="dl", adult="on", timeout=60,  filter='', verbose=True, png_conv=False):
+        self.download_count = 0
+        self.query = query
+        self.dirout = dirout
+        self.adult = adult
+        self.filter = filter
+        self.verbose = verbose
+        self.seen = set()
+        self.png_conv = png_conv
+        assert type(limit) == int, "limit must be integer"
+        self.limit = limit
+        assert type(timeout) == int, "timeout must be integer"
+        self.timeout = timeout
+
+        # self.headers = {'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0'}
+        self.page_counter = 0
+        self.headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) ' 
+      'AppleWebKit/537.11 (KHTML, like Gecko) '
+      'Chrome/23.0.1271.64 Safari/537.11',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+      'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+      'Accept-Encoding': 'none',
+      'Accept-Language': 'en-US,en;q=0.8',
+      'Connection': 'keep-alive'}
+
+
+    def get_filter(self, shorthand):
+            if shorthand == "line" or shorthand == "linedrawing":
+                return "+filterui:photo-linedrawing"
+            elif shorthand == "photo":
+                return "+filterui:photo-photo"
+            elif shorthand == "clipart":
+                return "+filterui:photo-clipart"
+            elif shorthand == "gif" or shorthand == "animatedgif":
+                return "+filterui:photo-animatedgif"
+            elif shorthand == "transparent":
+                return "+filterui:photo-transparent"
+            else:
+                return ""
+
+
+
+
+    def save_image(self, link, file_path, conv=False):
+
+        if os.path.isfile(file_path):
+            log('file exist skipping')
+            return 
+
+        request = urllib.request.Request(link, None, self.headers)
+        image   = urllib.request.urlopen(request, timeout=self.timeout).read()
+        
+        if not imghdr.what(None, image):
+            print('[Error]Invalid image, not saving {}\n'.format(link))
+            # raise ValueError('Invalid image, not saving {}\n'.format(link))
+
+        if conv:
+            image = remove(image)
+        with open(str(file_path), 'wb') as f:
+            f.write(image)
+
+    
+    def download_image(self, link):
+        self.download_count += 1
+        # Get the image link
+        try:
+            path      = urllib.parse.urlsplit(link).path
+            filename  = posixpath.basename(path).split('?')[0]
+            file_type = filename.split(".")[-1]
+            
+            if file_type.lower() not in ["jpe", "jpeg", "jfif", "exif", "tiff", "gif", "bmp", "png", "webp", "jpg"]:
+                file_type = "jpg"
+            
+            conv = False
+            if self.png_conv:
+                if file_type != "png":
+                    conv = True
+                    file_type = "png"
+            
+            if self.verbose:
+                print("[%] Downloading Image #{} from {}".format(self.download_count, link))
+                
+            self.save_image(link, self.dirout.joinpath(f"img_{self.download_count}.{file_type}"), conv)
+
+
+        except Exception as e:
+            self.download_count -= 1
+            print("[!] Issue getting: {}\n[!] Error:: {}".format(link, e))
+
+    
+    def run(self):
+        while self.download_count < self.limit:
+            if self.verbose:
+                print('\n\n[!!]Indexing page: {}\n'.format(self.page_counter + 1))
+            # Parse the page source and download pics
+            request_url = 'https://www.bing.com/images/async?q=' + urllib.parse.quote_plus(self.query) \
+                          + '&first=' + str(self.page_counter) + '&count=' + str(self.limit) \
+                          + '&adlt=' + self.adult + '&qft=' + ('' if self.filter is None else self.get_filter(self.filter)) \
+                          + "&sc=10-35" + "&cvid=95DD13A3E5D043939BBC3117C416C74B&cc=US&setlang=en-US&form=QBIR"
+            #request_url = f"https://www.bing.com/images/search?q={urllib.parse.quote_plus(self.query)}&qs=n&form=QBIR&sp=-1&lq=0&pq=logo%20bicycle%20in%20svg%20black%20and%20white&sc=10-35&cvid=95DD13A3E5D043939BBC3117C416C74B&ghsh=0&ghacc=0&first=1&cw=1349&ch=661"
+            request  = urllib.request.Request(request_url, None, headers=self.headers)
+            response = urllib.request.urlopen(request)
+            html = response.read().decode('utf8')
+            if html ==  "":
+                print("[%] No more images are available")
+                break
+            links = re.findall('murl&quot;:&quot;(.*?)&quot;', html)
+            if self.verbose:
+                print("[%] Indexed {} Images on Page {}.".format(len(links), self.page_counter + 1))
+
+
+            for link in links:
+                if self.download_count < self.limit and link not in self.seen:
+                    self.seen.add(link)
+                    self.download_image(link)
+
+            self.page_counter += 1
+        print("\n\n[%] Done. Downloaded {} images.".format(self.download_count))
+
+
+
+
+
+
+################################################################################################
 class GoogleImageScraper():
     def __init__(self, search_key, image_path="downloads", limit=1, headless=True, min_resolution=(0, 0), max_resolution=(1920, 1080), max_missed=10):
         #check parameter types
@@ -205,162 +369,6 @@ class GoogleImageScraper():
                 pass
         print("--------------------------------------------------")
         print("[INFO] Downloads completed. Please note that some photos were not downloaded as they were not in the correct format (e.g. jpg, jpeg, png)")
-
-
-def fetch_google(query, image_path="downloads", limit=10, headless=True, min_resolution=(0, 0), 
-                 max_resolution=(512, 512), max_missed=10):
-
-    from selenium import webdriver
-    from selenium.webdriver.chrome.options import Options
-    from selenium.webdriver.common.by import By
-    from selenium.webdriver.support.ui import WebDriverWait
-    from selenium.webdriver.support import expected_conditions as EC
-    from selenium.common.exceptions import NoSuchElementException 
-
-    googleI = GoogleImageScraper(query, image_path, limit, headless, min_resolution, max_resolution, max_missed)
-    googleI.download()
-    del googleI
-
-
-def fetch(query, limit=1, dirout='dl', adult_filter='on', 
-          force_replace=False, timeout=60, filter="", verbose=0, png_conv=1):
-
-    # engine = 'bing'
-    
-    image_dir = Path(dirout).joinpath(query).absolute()
-    os_makedirs(str(image_dir))
-
-    png_conv = True if png_conv == 1 else False 
-    verbose  = True if verbose  == 1 else False 
-
-        
-    print("[%] Downloading Images to {}".format(str(image_dir.absolute())))
-    bing = Bing(query, limit, image_dir, adult_filter, timeout, filter, verbose, png_conv)
-    bing.run()
-
-
-
-################################################################################################
-class Bing:
-    def __init__(self, query, limit, dirout="dl", adult="on", timeout=60,  filter='', verbose=True, png_conv=False):
-        self.download_count = 0
-        self.query = query
-        self.dirout = dirout
-        self.adult = adult
-        self.filter = filter
-        self.verbose = verbose
-        self.seen = set()
-        self.png_conv = png_conv
-        assert type(limit) == int, "limit must be integer"
-        self.limit = limit
-        assert type(timeout) == int, "timeout must be integer"
-        self.timeout = timeout
-
-        # self.headers = {'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0'}
-        self.page_counter = 0
-        self.headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) ' 
-      'AppleWebKit/537.11 (KHTML, like Gecko) '
-      'Chrome/23.0.1271.64 Safari/537.11',
-      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-      'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-      'Accept-Encoding': 'none',
-      'Accept-Language': 'en-US,en;q=0.8',
-      'Connection': 'keep-alive'}
-
-
-    def get_filter(self, shorthand):
-            if shorthand == "line" or shorthand == "linedrawing":
-                return "+filterui:photo-linedrawing"
-            elif shorthand == "photo":
-                return "+filterui:photo-photo"
-            elif shorthand == "clipart":
-                return "+filterui:photo-clipart"
-            elif shorthand == "gif" or shorthand == "animatedgif":
-                return "+filterui:photo-animatedgif"
-            elif shorthand == "transparent":
-                return "+filterui:photo-transparent"
-            else:
-                return ""
-
-
-
-
-    def save_image(self, link, file_path, conv=False):
-
-        if os.path.isfile(file_path):
-            log('file exist skipping')
-            return 
-
-        request = urllib.request.Request(link, None, self.headers)
-        image   = urllib.request.urlopen(request, timeout=self.timeout).read()
-        
-        if not imghdr.what(None, image):
-            print('[Error]Invalid image, not saving {}\n'.format(link))
-            # raise ValueError('Invalid image, not saving {}\n'.format(link))
-
-        if conv:
-            image = remove(image)
-        with open(str(file_path), 'wb') as f:
-            f.write(image)
-
-    
-    def download_image(self, link):
-        self.download_count += 1
-        # Get the image link
-        try:
-            path      = urllib.parse.urlsplit(link).path
-            filename  = posixpath.basename(path).split('?')[0]
-            file_type = filename.split(".")[-1]
-            
-            if file_type.lower() not in ["jpe", "jpeg", "jfif", "exif", "tiff", "gif", "bmp", "png", "webp", "jpg"]:
-                file_type = "jpg"
-            
-            conv = False
-            if self.png_conv:
-                if file_type != "png":
-                    conv = True
-                    file_type = "png"
-            
-            if self.verbose:
-                print("[%] Downloading Image #{} from {}".format(self.download_count, link))
-                
-            self.save_image(link, self.dirout.joinpath(f"img_{self.download_count}.{file_type}"), conv)
-
-
-        except Exception as e:
-            self.download_count -= 1
-            print("[!] Issue getting: {}\n[!] Error:: {}".format(link, e))
-
-    
-    def run(self):
-        while self.download_count < self.limit:
-            if self.verbose:
-                print('\n\n[!!]Indexing page: {}\n'.format(self.page_counter + 1))
-            # Parse the page source and download pics
-            request_url = 'https://www.bing.com/images/async?q=' + urllib.parse.quote_plus(self.query) \
-                          + '&first=' + str(self.page_counter) + '&count=' + str(self.limit) \
-                          + '&adlt=' + self.adult + '&qft=' + ('' if self.filter is None else self.get_filter(self.filter)) \
-                          + "&sc=10-35" + "&cvid=95DD13A3E5D043939BBC3117C416C74B&cc=US&setlang=en-US&form=QBIR"
-            #request_url = f"https://www.bing.com/images/search?q={urllib.parse.quote_plus(self.query)}&qs=n&form=QBIR&sp=-1&lq=0&pq=logo%20bicycle%20in%20svg%20black%20and%20white&sc=10-35&cvid=95DD13A3E5D043939BBC3117C416C74B&ghsh=0&ghacc=0&first=1&cw=1349&ch=661"
-            request  = urllib.request.Request(request_url, None, headers=self.headers)
-            response = urllib.request.urlopen(request)
-            html = response.read().decode('utf8')
-            if html ==  "":
-                print("[%] No more images are available")
-                break
-            links = re.findall('murl&quot;:&quot;(.*?)&quot;', html)
-            if self.verbose:
-                print("[%] Indexed {} Images on Page {}.".format(len(links), self.page_counter + 1))
-
-
-            for link in links:
-                if self.download_count < self.limit and link not in self.seen:
-                    self.seen.add(link)
-                    self.download_image(link)
-
-            self.page_counter += 1
-        print("\n\n[%] Done. Downloaded {} images.".format(self.download_count))
-
 
 
 
