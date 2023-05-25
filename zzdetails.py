3a46e7052aaa89ee743a2a03caad4df6d96963e8
PN <arita37@users.noreply.github.com>
2023-05-25 22:25:55 +0900

    ok

---
+++ zzdetails.py
@@ -0,0 +1,54 @@
+
+
+### Here is an example of how you can start a spot instance using AWS CDK in Python:
+
+from aws_cdk import (
+    aws_ec2 as ec2,
+    core,
+)
+
+class SpotInstanceStack(core.Stack):
+
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        # Create a VPC
+        vpc = ec2.Vpc(self, "VPC")
+
+        # Create a security group
+        security_group = ec2.SecurityGroup(
+            self, "SecurityGroup",
+            vpc=vpc,
+            description="Allow ssh access to ec2 instances",
+            allow_all_outbound=True
+        )
+        security_group.add_ingress_rule(
+            ec2.Peer.any_ipv4(),
+            ec2.Port.tcp(22),
+            "allow ssh access from the world"
+        )
+
+        # Define the spot instance request
+        spot_instance_request = ec2.CfnSpotFleet(
+            self, "SpotInstanceRequest",
+            spot_fleet_request_config_data=ec2.CfnSpotFleet.SpotFleetRequestConfigDataProperty(
+                iam_fleet_role="arn:aws:iam::123456789012:role/aws-ec2-spot-fleet-tagging-role",
+                target_capacity=1,
+                launch_specifications=[
+                    ec2.CfnSpotFleet.SpotFleetLaunchSpecificationProperty(
+                        image_id=ec2.MachineImage.latest_amazon_linux().get_image(self).image_id,
+                        instance_type="t3.micro",
+                        key_name="my-key-pair",
+                        security_groups=[security_group.security_group_id]
+                    )
+                ]
+            )
+        )
+Copy
+This code creates a VPC and a security group that allows SSH access. It then defines a spot instance request with the desired instance type and uses the latest Amazon Linux AMI. You will need to replace my-key-pair with the name of your own key pair and 123456789012 with your own AWS account ID.
+
+Is there anything else you would like to know?
+
+
+
+
